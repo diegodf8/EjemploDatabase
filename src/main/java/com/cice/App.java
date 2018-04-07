71bab20783642563fe069fcaa58a18579790f7bb
Diego Dominguez <diegodf8@gmail.com>
2018-04-07 12:19:38 +0200

    Prueba correcta crear tabla

--- src/main/java/com/cice/App.java
+++ src/main/java/com/cice/App.java
@@ -1,6 +1,9 @@
 package com.cice;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 public class App
 {
@@ -10,10 +13,26 @@ public class App
             // Cargar el driver
             Class.forName("com.mysql.jdbc.Driver");
 
-            // Generar conexion
-            Connection
+            // Generar conexion con mysql
+            Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:8889/pruebadb","root","root");
+
+            // Sentencia para atacar a la base de datos
+            Statement statement = connection.createStatement();
+
+            // Especificar la peticion
+            String sql ="CREATE TABLE prueba (id INT NOT NULL AUTO_INCREMENT, nombre VARCHAR(30) NOT NULL, PRIMARY KEY (id))";
+
+            // Ejecutar la peticion contra la bbdd
+            statement.executeUpdate(sql);
+
+            // Cerrar recursos
+            connection.close();
+
+
 
         } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (SQLException e) {
             e.printStackTrace();
         }
 
